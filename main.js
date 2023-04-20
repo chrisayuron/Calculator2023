268f4db3e056dd11a1fba4b1cd4cd7327c501127
Ronald <rrnewball@gmail.com>
2023-04-20 11:42:32 -0500

    versión 2 finalizada

--- main.js
+++ main.js
@@ -8,28 +8,42 @@ const botones=document.querySelector('.calculadora__botones')
 botones.addEventListener('click',(e)=>{
     switch(e.target.id){
         case 'suma': operando.textContent="+"
+                     num2.value=num1.value
+                     num1.value='' 
                      break;
+                     
         case 'rest': operando.textContent="-"
+        num2.value=num1.value   
+        num1.value=''
                      break;
         case 'mult': operando.textContent="*"
+        num2.value=num1.value   
+        num1.value=''
                      break;
         case 'divi': operando.textContent="/"
+        num2.value=num1.value   
+        num1.value=''
                      break;
+        case 'limpiar':
+            num1.value=null
+            num2.value=null
+            operando.textContent=''
+            display.textContent=''
     }
   
     if(e.target.id=='igual'){
         if(!(isNaN(num1.value)) && !(isNaN(num2.value))){
             if(operando.textContent=="+"){
-                resultado=+num1.value + +num2.value
+                resultado=+num2.value + +num1.value
                 display.textContent= resultado
             }else if(operando.textContent=="-"){
-                resultado=+num1.value - +num2.value
+                resultado=+num2.value - +num1.value
                 display.textContent= resultado
             }else if(operando.textContent=="*"){
-                resultado=+num1.value * +num2.value
+                resultado=+num2.value * +num1.value
                 display.textContent= resultado
             }else if(operando.textContent=="/"){
-                    resultado=+num1.value  / +num2.value
+                    resultado=+num2.value  / +num1.value
                     display.textContent= resultado
         }
     }
