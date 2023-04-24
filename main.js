193b638f2e230637a33487b0c9005ef6eff825f1
Ronald <rrnewball@gmail.com>
2023-04-24 06:15:28 -0500

    finish

--- main.js
+++ main.js
@@ -1,82 +1,73 @@
-const num1=document.getElementById('num1')
-const num2=document.getElementById('num2')
-const operando=document.getElementById('operando')
-const display=document.getElementById('display')
-
-for(i=0;i<=9;i++){
-    document.getElementById('btn'+i).addEventListener('click',()=>{
-        console.log(e.target.value)
-    })
-}
-
 const botones=document.querySelector('.calculadora__botones')
+const display=document.getElementById('display')
+let num1=null
+let operador=null
+
+for (let i = 0; i <= 9; i++) { 
+    //creo un evento click para cada boton numérico
+    document.getElementById(`btn${i}`).addEventListener('click', (e)=>{
+        mostrarTecla(e.target.value)
+    }); 
+}
 window.addEventListener('keydown',(e)=>{
-    console.log(e.key)
-    if(!isNaN(e.key)){
-        mostrar(e.key)
-       
+    //creo un evento para escuchar la pulsación de las teclas
+    if(!isNaN(e.key) || (e.key=='.')){
+        mostrarTecla(e.key)
     }else if(e.key=='Backspace'){
         let texto=Array.from(display.textContent)
         console.log(typeof(texto))
         texto.pop()
         texto=texto.join('')
         display.textContent=texto
+    }else if (e.key=='Enter'){
+        mostrarResultado(num1,operador)
+    }else if(e.key=='Escape'){
+        limpiarDisplay()
+    }else if(e.key=='+' || e.key=='-' || e.key=='*' || e.key=='/'){
+        num1=+display.textContent
+        operador=e.key
+        display.textContent=0
     }
-})
+})   
 
-botones.addEventListener('click',(e)=>{
-    
-})
-
-function mostrar(tecla){
-    display.textContent+=tecla
+function mostrarTecla(tecla){
+    if (display.textContent==0){
+        display.textContent=tecla
+    }else if(tecla=='.' && display.textContent.includes('.')){
+        return
+    }else{
+        display.textContent+=tecla
+    }  
 }
 
+function limpiarDisplay(){
+    display.textContent=0
+}
 
+function mostrarResultado(numAnt,ope){
+    console.log(ope)
+    console.log(numAnt)
+    if(ope=='+'){
+        display.textContent=numAnt + +display.textContent
+    }else if (ope=='-'){
+        display.textContent=numAnt - +display.textContent
+    }else if(ope=='*'){
+        display.textContent=numAnt * +display.textContent
+    }else if(ope=='/'){
+        display.textContent=numAnt / +display.textContent
+    }
+}
 
-// botones.addEventListener('click',(e)=>{
-//     switch(e.target.id){
-//         case 'suma': operando.textContent="+"
-//                      num2.value=num1.value
-//                      num1.value='' 
-//                      break;
-                     
-//         case 'rest': operando.textContent="-"
-//         num2.value=num1.value   
-//         num1.value=''
-//                      break;
-//         case 'mult': operando.textContent="*"
-//         num2.value=num1.value   
-//         num1.value=''
-//                      break;
-//         case 'divi': operando.textContent="/"
-//         num2.value=num1.value   
-//         num1.value=''
-//                      break;
-//         case 'limpiar':
-//             num1.value=null
-//             num2.value=null
-//             operando.textContent=''
-//             display.textContent=''
-//     }
-  
-//     if(e.target.id=='igual'){
-//         if(!(isNaN(num1.value)) && !(isNaN(num2.value))){
-//             if(operando.textContent=="+"){
-//                 resultado=+num2.value + +num1.value
-//                 display.textContent= resultado
-//             }else if(operando.textContent=="-"){
-//                 resultado=+num2.value - +num1.value
-//                 display.textContent= resultado
-//             }else if(operando.textContent=="*"){
-//                 resultado=+num2.value * +num1.value
-//                 display.textContent= resultado
-//             }else if(operando.textContent=="/"){
-//                     resultado=+num2.value  / +num1.value
-//                     display.textContent= resultado
-//         }
-//     }
-// }
-           
-    
-// })
+botones.addEventListener('click',(e)=>{
+    if(e.target.id=='limpiar'){
+            limpiarDisplay()
+    }else if(e.target.id=='clear'){
+        limpiarDisplay()           
+    }else if (e.target.id=='igual'){
+      mostrarResultado(num1,operador)
+    }else if(e.target.id=='+' || e.target.id=='-' || e.target.id=='*' || e.target.id=='/'){
+        num1=+display.textContent
+        operador=e.target.id
+        display.textContent=0
+    }
+})
